2de56103e76e402129713595db23df472257c6ce
Cadu Braga <carlos_e_braga@estudante.sesisenai.org.br>
2025-04-16 23:43:03 -0300

    Subindo arquivos

--- Aluno.java
+++ Aluno.java
@@ -1,36 +1,75 @@
-public class Aluno {
+import java.util.Scanner;
 
+public class Aluno {
 	private String nome;
-	private int matricula;
-	private double notaFinal;
+	private int idade;
+	private String matricula;
+	private int anoIngresso;
 
 	public Aluno() {
 		this.nome = "";
-		this.matricula = 0;
-		this.notaFinal = 0.0;
+		this.idade = 0;
+		this.matricula = "";
+		this.anoIngresso = 0;
 	}
 
-	public String getNome() {
-		return nome;
+	public Aluno(String nome, int idade, String matricula, int anoIngresso) {
+		this.nome = nome;
+		this.idade = idade;
+		this.matricula = matricula;
+		this.anoIngresso = anoIngresso;
 	}
 
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
-	public int getMatricula() {
-		return matricula;
+	public String getNome() {
+		return nome;
 	}
 
-	public void setMatricula(int matricula) {
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setMatricula(String matricula) {
 		this.matricula = matricula;
 	}
 
-	public double getNotaFinal() {
-		return notaFinal;
+	public String getMatricula() {
+		return matricula;
 	}
 
-	public void setNotaFinal(double notaFinal) {
-		this.notaFinal = notaFinal;
+	public void setAnoIngresso(int anoIngresso) {
+		this.anoIngresso = anoIngresso;
+	}
+
+	public int getAnoIngresso() {
+		return anoIngresso;
+	}
+
+	public void preencherDados(Scanner scanner) {
+		System.out.print("Nome: ");
+		this.nome = scanner.nextLine();
+		System.out.print("Idade (anos): ");
+		this.idade = scanner.nextInt();
+		scanner.nextLine();
+		System.out.print("Matrícula: ");
+		this.matricula = scanner.nextLine();
+		System.out.print("Ano de ingresso: ");
+		this.anoIngresso = scanner.nextInt();
+		scanner.nextLine();
+	}
+
+	public void imprimirDados() {
+		System.out.println("Informações do Aluno:");
+		System.out.println("Nome: " + getNome());
+		System.out.println("Idade: " + getIdade() + " anos");
+		System.out.println("Matrícula: " + getMatricula());
+		System.out.println("Ano de Ingresso: " + getAnoIngresso());
 	}
 }
--- Livro.java
+++ Livro.java
@@ -1,36 +1,74 @@
+import java.util.Scanner;
+
 public class Livro {
+	private String titulo;
+	private String autor;
+	private int paginas;
+	private double preco;
 
-    private String titulo;
-    private String autor;
-    private int anoDePublicacao;
+	public Livro() {
+		this.titulo = "";
+		this.autor = "";
+		this.paginas = 0;
+		this.preco = 0.0;
+	}
 
-    public Livro() {
-        this.titulo = "";
-        this.autor = "";
-        this.anoDePublicacao = 0;
-    }
+	public Livro(String titulo, String autor, int paginas, double preco) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.paginas = paginas;
+		this.preco = preco;
+	}
 
-    public String getTitulo() {
-        return titulo;
-    }
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+	public String getTitulo() {
+		return titulo;
+	}
 
-    public String getAutor() {
-        return autor;
-    }
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
 
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
+	public String getAutor() {
+		return autor;
+	}
 
-    public int getAnoDePublicacao() {
-        return anoDePublicacao;
-    }
+	public void setPaginas(int paginas) {
+		this.paginas = paginas;
+	}
 
-    public void setAnoDePublicacao(int anoDePublicacao) {
-        this.anoDePublicacao = anoDePublicacao;
-    }
+	public int getPaginas() {
+		return paginas;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void preencherDados(Scanner scanner) {
+		System.out.print("Título: ");
+		this.titulo = scanner.nextLine();
+		System.out.print("Autor: ");
+		this.autor = scanner.nextLine();
+		System.out.print("Número de páginas: ");
+		this.paginas = scanner.nextInt();
+		System.out.print("Preço: R$");
+		this.preco = scanner.nextDouble();
+		scanner.nextLine();
+	}
+
+	public void imprimirDados() {
+		System.out.println("Informações do Livro:");
+		System.out.println("Título: " + getTitulo());
+		System.out.println("Autor: " + getAutor());
+		System.out.println("Número de páginas: " + getPaginas());
+		System.out.printf("Preço: R$%.2f\n", getPreco());
+	}
 }
